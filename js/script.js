7bd35eb79d234f714864aa2759b113e489629731
MUHAMMED IRSHAD K <69687099+irshad-alungal@users.noreply.github.com>
2022-07-22 10:03:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ==================== typing animation ====================*/
+var typed = new Typed(".typing",{
+    strings:["","Mechanical Engineer","MEP Designer","Product Designer","Graphic Designer","Web Designer","Motion Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
