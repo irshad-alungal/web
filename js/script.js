8fb1dadb4bcee12c84fbc9627594d2d0649332d3
MUHAMMED IRSHAD K <69687099+irshad-alungal@users.noreply.github.com>
2023-06-03 07:51:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ==================== typing animation ====================*/
 var typed = new Typed(".typing",{
-    strings:["","Mechanical Engineer","MEP Designer","Product Designer","Graphic Designer","Web Designer","Motion Graphic Designer"],
+    strings:["front-end developer","Mechanical Engineer","MEP Designer","Product Designer","Graphic Designer","Web Designer","Motion Graphic Designer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
